b4435640125037356226a6656cc545ff8b0fa25b
douglaslx27 <douglaslx27@gmail.com>
2022-09-29 19:35:42 -0300

    Arquivo de cores para o projeto

---
+++ salesRecord/componentes/colors.js
@@ -0,0 +1,14 @@
+export default {
+
+    backgroundContainer: '#7FFFD4',
+    backgroundSubContainer: '#00FA9A',
+    colorTitle: '#2F4F2F',
+    colorText: '#238E23',
+    btn: '#00FA9A',
+    textBtn: '#238E23',
+    borderColorInputFalse: '#060',
+    borderColorInputTrue: '#9f9',
+    backgroundInputTrue: '#fff',
+    underlayColorBtn: '#7FFFD4'
+
+}
